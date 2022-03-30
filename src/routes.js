6e47b29e1258ea2b3373f7a178a2a783b0335e8e
ArmandoCast <armandoneto18@gmail.com>
2022-03-30 11:06:18 -0300

    chore: add routes

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Character from './pages/Character';
+
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route exact path="/character">
+        <Character />
+      </Route>
+    </Switch>
+  </Router>
+);
+
+export default Routes;
